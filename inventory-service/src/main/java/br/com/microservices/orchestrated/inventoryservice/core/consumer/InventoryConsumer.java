82593c8d565e71cce6b1666bacb4408ddf0f1957
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-05-27 15:03:16 -0300

    chore: create project from orchestrator saga

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/consumer/InventoryConsumer.java
@@ -0,0 +1,37 @@
+package br.com.microservices.orchestrated.inventoryservice.core.consumer;
+
+import br.com.microservices.orchestrated.inventoryservice.core.service.InventoryService;
+import br.com.microservices.orchestrated.inventoryservice.core.utils.JsonUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class InventoryConsumer {
+
+  private final JsonUtil jsonUtil;
+  private final InventoryService inventoryService;
+
+  @KafkaListener(
+          groupId = "${spring.kafka.consumer.group-id}",
+          topics = "${spring.kafka.topic.inventory-success}"
+  )
+  public void consumerSuccessEvent(String payload) {
+    log.info("Receiving success event {} from inventory-success topic", payload);
+    var event = jsonUtil.toEvent(payload);
+    inventoryService.updateInventory(event);
+  }
+
+  @KafkaListener(
+          groupId = "${spring.kafka.consumer.group-id}",
+          topics = "${spring.kafka.topic.inventory-fail}"
+  )
+  public void consumerFailEvent(String payload) {
+    log.info("Receiving rollback event {} from inventory-fail topic", payload);
+    var event = jsonUtil.toEvent(payload);
+    inventoryService.rollbackInventory(event);
+  }
+}
